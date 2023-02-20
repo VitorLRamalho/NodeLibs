0d3f8b46a33a0b4aec05e68ef8a8752e5bd0ca9e
Vitor Luiz Ramalho <vitorlramalho@gmail.com>
2023-02-20 11:51:56 -0300

    commit

--- index.js
+++ index.js
@@ -2,18 +2,24 @@ import fs from "fs";
 import chalk from "chalk";
 
 function treatErr(err) {
-    throw new error(err);
+    throw new Error(chalk.red(err.code, 'não há arquivo no diretório'));
 }
 
 function getArch(archWay) {
     const encoding = 'utf-8';
+    fs.promises.readFile(archWay, encoding)
+    .then((text) => console.log(chalk.yellow(text)))
+    .catch(treatErr);
+}
+
+/* function getArch(archWay) {
+    const encoding = 'utf-8';
     fs.readFile(archWay, encoding, (err, text) => {
         if(err){
             treatErr(err)
-        }else{
-            console.log(chalk.red(text));
         }
+        console.log(chalk.green(text));
     });
-}
+} */
 
 getArch('./md/texto.md');
